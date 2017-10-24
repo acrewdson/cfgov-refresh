598e9350cb3698db2c377d1ff9b45ac7624b2ca5
Hillary Jeffrey <hillary.jeffrey@cfpb.gov>
2017-10-24 16:22:59 -0400

    Remove codecov coverage reporting

--- run_travis.sh
+++ run_travis.sh
@@ -7,16 +7,13 @@ echo "running $RUNTEST tests"
 if [ "$RUNTEST" == "frontend" ]; then
     source $HOME/.nvm/nvm.sh
     gulp "test" --travis
-    bash <(curl -s https://codecov.io/bash) -F frontend
 elif [ "$RUNTEST" == "backend" ]; then
     tox -e lint
     tox -e fast
     tox -e missing-migrations
-    bash <(curl -s https://codecov.io/bash) -F backend
 elif [ "$RUNTEST" == "backend3" ]; then
     tox -e lint-py3
     tox -e fast-py3
-    bash <(curl -s https://codecov.io/bash) -F backend
 elif [ "$RUNTEST" == "acceptance" ]; then
     source $HOME/.nvm/nvm.sh
     export DISPLAY=:99.0
